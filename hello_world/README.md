6c53dcfd741142a5c1683cce0f80d766328c1904
nharsch <nigelharsch@gmail.com>
2016-04-02 00:32:03 -0400

    clean up output folder from git and REAMDE

---
+++ hello_world/README.md
@@ -0,0 +1,9 @@
+
+## Notes
+
+clojure transpiles clojurescript to javascript
+
+google closure library managers JS dependencies
+
+clojure comes with watch tool that will watch for file changes to kick off new builds
+
--- hello_world/out/hello_world/core.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.8.41 {}
-goog.provide('hello_world.core');
-goog.require('cljs.core');
-cljs.core.enable_console_print_BANG_.call(null);
-cljs.core.println.call(null,"Hello world!");
-cljs.core.println.call(null,"Oh that's just me, making changes");
-
-//# sourceMappingURL=core.js.map
